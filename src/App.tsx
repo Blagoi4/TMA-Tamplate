10b299f1bddbc1da30a8e07c7a06d8ce76ab8615
Blagoi4 <vladislavblagoiwork@gmail.com>
2024-05-02 12:20:57 +0300

    Revert "test project"
    This reverts commit dc858e1c1f21be42bc0d06744405c81b367f24f4.

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,60 @@ import "./App.css";
 import { CHAIN, TonConnectButton } from "@tonconnect/ui-react";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { useCounterContract } from "./hooks/useCounterContract";
-
-import "@twa-dev/sdk";
-import AccountInfo from "./components/accountInfo";
+import { HttpClient, Api, Account } from "tonapi-sdk-js";
+import { useEffect } from "react";
 
 function App() {
   const { connected, network } = useTonConnect();
   const { value, address, sendIncrement } = useCounterContract();
-  // const [balance, setBalance] = useState(0)
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const token =
+        "AFFOSTQDZOETPHQAAAAJUQPLAAXFLUJ6KZA7GZHFOYCADVDZ5FRTXO35LCI3DZFDACDB4ZA";
+
+      const httpClient = new HttpClient({
+        baseUrl: "https://tonapi.io",
+        baseApiParams: {
+          headers: {
+            "content-length": "104",
+            Authorization: `Bearer ${token}`,
+            "Content-type": "application/json; charset=utf-8",
+          },
+        },
+      });
+
+      const client = new Api(httpClient);
+
+      const address = "UQC27V3iUgelvnGZChU9PV-XUYO6MEo5l5MnPZIn4XoaK0XN";
+
+      try {
+        const accountInfo: Account = await client.accounts.getAccount(address);
+        console.log(accountInfo);
+      } catch (error) {
+        console.error("Error fetching jetton info:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  console.log();
   return (
     <div className="App">
-      <div className="network">
+      <div>
         {network ? (network === CHAIN.MAINNET ? "mainnet" : "testnet") : "N/A"}
       </div>
       <div className="Container">
         <TonConnectButton />
+        <div className="Card">
+          <b>Counter Address</b>
+          <div className="Hint">{address?.slice(0, 30) + "..."}</div>
+        </div>
 
-        {connected ? <AccountInfo />: 'IIIIIII'}
+        <div className="Card">
+          <b>Counter Value</b>
+          <div>{value ?? "Loading..."}</div>
+        </div>
 
         <a
           className={`Button ${connected ? "Active" : "Disabled"}`}
@@ -30,7 +66,6 @@ function App() {
           Increment
         </a>
       </div>
-      {/* <AccountInfo/> */}
     </div>
   );
 }
