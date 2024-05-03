e88624e258235663218b0f2de5529267f34a1956
Blagoi4 <vladislavblagoiwork@gmail.com>
2024-05-03 11:54:58 +0300

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = "https://Blagoi4.github.io/my-twa/tonconnect-manifest.json";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </TonConnectUIProvider>,
-)
+  </TonConnectUIProvider>
+);
