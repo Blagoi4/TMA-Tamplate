4adfdcb0087f1a3253ce1d75381ead2e198277bf
Blagoi4 <vladislavblagoiwork@gmail.com>
2024-05-03 13:41:36 +0300

    testing

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,12 @@ import "./index.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 // const manifestUrl = '/public/tonconnect-manifest.json';
-// const manifestUrl =
-  // "https://github.com/Blagoi4/TWA-Template/blob/main/tonconnect-manifest.json";
 // const manifestUrl = 'https://Blagoi4.github.io/my-twa/tonconnect-manifest.json';
+const manifestUrl = 'https://Blagoi4.github.io/TWA-Template/tonconnect-manifest.json';
 ///TESTING MODEL
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <TonConnectUIProvider >
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
   </TonConnectUIProvider>
 );
